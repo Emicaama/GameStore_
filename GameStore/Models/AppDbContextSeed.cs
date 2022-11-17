289529fa5dcc1c2977610c7e4abe152eff94924a
Emicaama <69168104+Emicaama@users.noreply.github.com>
2022-11-17 17:25:58 -0300

    finish

---
+++ GameStore/Models/AppDbContextSeed.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace GameStore.Models
+{
+    public static class AppDbContextSeed
+    {
+        public static async Task SeedRoleAsync(RoleManager<IdentityRole> roleManager)
+        {
+            await roleManager.CreateAsync(new IdentityRole(Roles.Admin));
+            await roleManager.CreateAsync(new IdentityRole(Roles.User));
+        }
+
+        public static async Task SeedAdminAsync(UserManager<IdentityUser> userManager)
+        {
+            var userAdmin = userManager.Users.Where(x => x.Email == Roles.MailAdmin).FirstOrDefault();
+            if (userAdmin != null) return;
+
+            userAdmin = new IdentityUser
+            {
+                UserName = Roles.MailAdmin,
+                Email = Roles.MailAdmin,
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = false,
+                TwoFactorEnabled = false,
+                LockoutEnabled = true,
+                AccessFailedCount = 0
+            };
+            await userManager.CreateAsync(userAdmin, "ex8eS%08MQ7I");
+            await userManager.AddToRoleAsync(userAdmin, Roles.Admin);
+        }
+    }
+}
---
+++ GameStore/Models/Roles.cs
@@ -0,0 +1,10 @@
+﻿namespace GameStore.Models
+{
+    public static class Roles
+    {
+        public const string Admin = "Admin";
+        public const string User = "Usuario";
+
+        public const string MailAdmin = "Admin@gmail.com";
+    }
+}
